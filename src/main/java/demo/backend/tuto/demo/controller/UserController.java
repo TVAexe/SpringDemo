92d7ab3d6ab3c8b4b881ee459fb20bb8e3791a30
tvaexe <vietanh.vadc.2004@gmail.com>
2025-08-21 11:45:51 +0700

    exception demo

--- src/main/java/demo/backend/tuto/demo/controller/UserController.java
+++ src/main/java/demo/backend/tuto/demo/controller/UserController.java
@@ -2,19 +2,13 @@ package demo.backend.tuto.demo.controller;
 
 import java.util.List;
 
+import demo.backend.tuto.demo.service.exception.IdInvalidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import demo.backend.tuto.demo.domain.User;
 import demo.backend.tuto.demo.service.UserSevice;
-import org.springframework.web.bind.annotation.PutMapping;
-
 
 
 @RestController
@@ -32,8 +26,16 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
+    @ExceptionHandler(value = IdInvalidException.class)
+    public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException) {
+        return ResponseEntity.badRequest().body(idInvalidException.getMessage());
+    }
+
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+        if (id>=1500) {
+            throw new IdInvalidException("Id khong lon hon 1500");
+        }
         this.userService.handleDeleteUser(id);
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
     }
---
+++ src/main/java/demo/backend/tuto/demo/service/exception/IdInvalidException.java
@@ -0,0 +1,7 @@
+package demo.backend.tuto.demo.service.exception;
+
+public class IdInvalidException extends Exception {
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
